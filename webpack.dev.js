1364c1fd60e419a44661d59d9f0c4d11a46cde16
Ty Everett <ty@tyweb.us>
2022-10-20 12:45:58 -0700

    Open on dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
+    open: true,
     port: 8088, // you can change the port
     client: {
       overlay: true // Show application errors
